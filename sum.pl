cf0bd335287a4a501809f04f2b484c12814d0f8c
antoniochandra <m26415171@john.petra.ac.id>
2016-09-16 00:07:20 +0700

    belajar perl

---
+++ sum.pl
@@ -0,0 +1,12 @@
+use strict;
+use warnings;
+     
+    # given a file with a number on each row, print the sum of the numbers
+     
+    my $sum = 0;
+    my $filename = "numbers.txt";
+    open(my $fh, "<", $filename) or die "Could not open '$filename'\n";
+    while (my $line = <$fh>) {
+        $sum += $line;
+    }
+    print "hasil penjumlahan : $sum\n";
