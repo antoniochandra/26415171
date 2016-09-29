f407f804d599f8de28f1dbaeb918e8ee181a7248
antoniochandra <m26415171@john.petra.ac.id>
2016-09-29 08:33:17 +0700

    tugas5

---
+++ cekganjilgenap.pl
@@ -0,0 +1,7 @@
+print "Input Angka : ";
+$angka=<STDIN>;
+
+if($angka % 2 == 0){
+print "$angka Merupakan Bilangan Genap\n";}
+else{
+print "$angka Merupakan Bilangan Ganjil\n";}
---
+++ coba.pl
@@ -0,0 +1,16 @@
+print "2+3 = ";
+print  2+3;
+print "\n";
+print "5.1-2.4 = ";
+print 5.1-2.4;
+print "\n";
+print "3*12 = ";
+print 3*12;
+printf "\n";
+printf "12/3 = "; 
+print 12/3;
+print "\n";
+print "12%3 = ";
+print 12%3;
+printf "\n";
+print "2\n"x3;
